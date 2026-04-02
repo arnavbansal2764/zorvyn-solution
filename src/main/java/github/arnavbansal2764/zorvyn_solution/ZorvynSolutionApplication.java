281fe743dafc7f3d12db4defa4a5281e21596770
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2026-04-02 10:43:36 +0530

    Initial commit

---
+++ src/main/java/github/arnavbansal2764/zorvyn_solution/ZorvynSolutionApplication.java
@@ -0,0 +1,13 @@
+package github.arnavbansal2764.zorvyn_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorvynSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZorvynSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/arnavbansal2764/zorvyn_solution/ZorvynSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package github.arnavbansal2764.zorvyn_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
